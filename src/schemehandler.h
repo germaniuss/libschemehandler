f305fbf62bbd3b20f96b033ca4866811091214a8
germaniuss <germanchik01atalyants@gmail.com>
2022-08-20 20:45:53 +0200

    Add windows uri scheme registering
    calling the second instance with "explore" does not correctly open the pipe

--- src/schemehandler.h
+++ src/schemehandler.h
@@ -4,11 +4,17 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+#if defined(_WIN32) || defined(_WIN64)
+#include <tchar.h>
+#else
+
+#endif
+
 #define SCHEME_HANDLER_VERSION "0.7.2"
 
 typedef struct scheme_handler scheme_handler;
 
-bool scheme_register(const char* protocol, const char* handler, bool terminal);
+bool scheme_register(const char* protocol, const char* exec, bool terminal);
 bool scheme_open(const char* url);
 scheme_handler* app_open(int argc, char* argv[], const char* dir, void* (*callback)(void* data, const char* value), void* data);
 
