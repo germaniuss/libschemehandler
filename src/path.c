13296a0037181094b23cdca628245566386002b9
germaniuss <germanchik01atalyants@gmail.com>
2022-08-22 14:04:18 +0200

    Fixes for windows

--- src/path.c
+++ src/path.c
@@ -28,6 +28,7 @@ char* getcurrentdir() {
 }
 
 void path_add(char** path, const char* dir) {
+    if (dir == NULL) return;
     if (*dir == 'C' || *path == NULL) {
         str_destroy(path);
         *path = str_create(dir);
